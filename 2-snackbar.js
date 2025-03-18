4fedff822569f0bcfaad57a5f79311ac05e14da4
ulia012008 <ulia012008@gmail.com>
2025-03-18 11:43:51 +0000

    Deploying to gh-pages from @ ulia012008/goit-js-hw-10@657c70a1151da0824282caffd5955fab00b384c7 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i as s}from"./assets/vendor-njWUcVeN.js";document.querySelector(".form").addEventListener("submit",function(i){i.preventDefault();const t=Number(this.elements.delay.value),o=this.elements.state.value;new Promise((e,r)=>{setTimeout(()=>{o==="fulfilled"?e(t):r(t)},t)}).then(e=>{s.success({title:"✅ Success",message:`Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{s.error({title:"❌ Error",message:`Rejected promise in ${e}ms`,position:"topRight"})})});
+//# sourceMappingURL=2-snackbar.js.map
